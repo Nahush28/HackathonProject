a64ec87eb056e43a8eecbd394a10ae5d372d9a8f
Michael Con <michaelcon@Michaels-MacBook-Pro.local>
2020-10-17 10:41:00 -0600

    Added the panel for the game

---
+++ src/Package/GamePanel.java
@@ -0,0 +1,15 @@
+package Package;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+   //private static final long serialVersionUID;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    public GamePanel(){
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+    }
+}
