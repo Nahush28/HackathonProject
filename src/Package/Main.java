2e8350084453ef47efbd6a580fc582edbd6e419e
Nahush28 <nwalveka@purdue.edu>
2020-10-17 14:14:18 -0400

    test deletion of hello world print statement.

--- src/Package/Main.java
+++ src/Package/Main.java
@@ -6,7 +6,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         JFrame frame = new JFrame("Platform");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
